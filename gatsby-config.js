2bbacaf6d76c1b44c239e5783b3f34a78efadefc
tien_oii <nichapon.didwisade@ofitechnology.com>
2021-01-04 16:04:41 +0700

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,19 +3,19 @@ module.exports = {
     "gatsby-plugin-styled-components",
     {
       resolve: "gatsby-source-strapi",
-      // options: {
-      //   apiURL: "http://localhost:1337",
-      //   contentTypes: ["category", "user"],
-      //   singleTypes: ["test-type"],
-      //   queryLimit: 1000,
-      // },
-
       options: {
-        apiURL: "https://oii-demobackend.tk",
-        contentTypes: ["user"],
-        singleTypes: ["homepage", "navigation", "site-setting"],
+        apiURL: "http://localhost:1337",
+        contentTypes: ["category", "user"],
+        singleTypes: ["test-type"],
         queryLimit: 1000,
       },
+
+      // options: {
+      //   apiURL: "https://oii-demobackend.tk",
+      //   contentTypes: ["user"],
+      //   singleTypes: ["homepage", "navigation", "site-setting"],
+      //   queryLimit: 1000,
+      // },
 
     },
   ],  
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react"
-// import { StaticQuery, graphql } from 'gatsby';
-// import axios from 'axios'
+import { StaticQuery, graphql } from 'gatsby';
+import axios from 'axios'
 
 // styles
 const pageStyles = {
@@ -9,60 +9,57 @@ const pageStyles = {
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
 
-// state = {
-//   siteSetting: null
-// }
-
-// async componentDidMount(){
-//   await axios
-//     .get(SERVER_URL + "/site-setting")
-//     .then(response => {
-//       console.log("get site-setting response", response.data);
-//       this.setState({ siteSetting: response.data });
-//     })
-//     .catch(error => console.log("Error when connect to API 3", error));
-// }
-
-// const query = graphql`
-//   query {
-//     allStrapiUser {
-//       edges {
-//         node {
-//           strapiId
-//         }
-//       }
-//     }
-//   }
-// `;
+// test query by Graphql
+const query = graphql`
+  query {
+    strapiSitesetting {
+      id
+    }
+  }
+`;
 
 // markup
 const IndexPage = () => {
 
-  // console.log('>>>>> test query: ', query)
-  // let SERVER_URL = "https://oii-demobackend.tk";
+  // test queryy by axios
+  React.useEffect( () => {
+    let SERVER_URL = "https://oii-demobackend.tk";
 
-  // await axios
-  //   .get(SERVER_URL + "/site-setting")
-  //   .then(response => {
-  //     console.log("get site-setting response", response.data);
-  //   })
-  //   .catch(error => console.log("Error when connect to API 3", error));
+    async function fetchData() {
+      // You can await here
+      await axios
+        .get(SERVER_URL + "/site-setting")
+        .then(response => {
+          console.log("get site-setting response", response.data);
+        })
+        .catch(error => console.log("Error when connect to API 3", error));
+    }
+
+    fetchData()
+    
+  }, [])
+
+  
 
   return (
     <main style={pageStyles}>
       <title>Home Page</title>
       <h1>Test Query</h1>
 
-      {/* <StaticQuery
+      <StaticQuery
         query={query}
         render={data => (
+          // <ul>
+          //   {data.allStrapiCategory.edges.map(category => (
+          //     <li key={category.node.strapiId}>{category.node.Name} : {category.node.Description}</li>
+          //   ))}
+          // </ul>
+
           <ul>
-            {data.allStrapiCategory.edges.map(category => (
-              <li key={category.node.strapiId}>{category.node.Name} : {category.node.Description}</li>
-            ))}
+            {data.strapiSitesetting.id}
           </ul>
         )}
-      /> */}
+      />
     </main>
   )
 }
